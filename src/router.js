0b06dc2d891d20717718176fb51c8bc9a12d45fe
周宇雄 <1518204079@qq.com>
2019-06-20 19:12:19 +0800

    登录页面路由完成

---
+++ src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Login from '@/views/login.vue'
+
+Vue.use(VueRouter)
+
+var router = new VueRouter({
+  routes: [
+    {
+      name: 'Login',
+      path: '/login',
+      component: Login
+    }
+  ]
+})
+
+export default router
